ffb210c6afb637214d933dcedc568421b9ceff64
Kiki Jeyakumar <Kiki Jeyakumar@78.34.46.36>
2016-07-06 14:48:16 +0200

    Server und Client Main

--- src/pp2016/team13/client/comm/ServerMain.java
+++ src/pp2016/team13/client/comm/ServerMain.java
@@ -1,5 +1,11 @@
 package pp2016.team13.client.comm;
 
+import pp2016.team13.client.comm.Server;
+
 public class ServerMain {
+
+	public static void main(String[] args){
+		new Server(2345);
+	}
 
 }
