a1ebd9243a30b60ffd18dc677e2a3019d23148fb
Marius <Marius>
2016-07-08 18:41:57 +0200

    Merge Fehler beseitigt

--- src/pp2016/team13/client/comm/ServerMain.java
+++ src/pp2016/team13/client/comm/ServerMain.java
@@ -1,20 +1,13 @@
 package pp2016.team13.client.comm;
 
-<<<<<<< HEAD
 import pp2016.team13.client.comm.Server;
-=======
->>>>>>> branch 'master' of https://github.com/HindiBones/project.git
+
 
 public class ServerMain {
 
 	public static void main(String[] args){
-<<<<<<< HEAD
+
 		new Server(2345);
 	}
 
-=======
-		new Server(13001);
-	}
-	
->>>>>>> branch 'master' of https://github.com/HindiBones/project.git
 }
