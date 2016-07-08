228996305a6a18f0cb3caf368fee7b2b8b360d1a
Marius <Marius>
2016-07-08 19:22:59 +0200

    Fix

--- src/pp2016/team13/client/engine/LevelNachricht.java
+++ src/pp2016/team13/client/engine/LevelNachricht.java
@@ -14,7 +14,6 @@ public class LevelNachricht extends Nachricht implements Serializable{
 		super(6);
 		for (int k = 0; k<5; k++)
 		{
-			
 			for (int i = 0; i<15; i++){
 				for (int j = 0; j<15 ; j++){
 					this.x[k].levelInhalt[i][j] = levelDaten[k][i][j];
