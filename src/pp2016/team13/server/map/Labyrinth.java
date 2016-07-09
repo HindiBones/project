2825eeb2ec261cdd2015dae6e91052c1bfb73598
Julius <Julius@Julius-PC.fritz.box>
2016-07-09 21:43:24 +0200

    akt

--- src/pp2016/team13/server/map/Labyrinth.java
+++ src/pp2016/team13/server/map/Labyrinth.java
@@ -42,7 +42,6 @@ public class Labyrinth {
 		double zufallsZahl = (Math.random()*zaehler)-1;
 		int zz1 = (int) zufallsZahl;
 		map[bodenplatten[zz1].xKoordinate][bodenplatten[zz1].yKoordinate]= 2;
-<<<<<<< HEAD
 		
 
 			// Position  Tuer
@@ -58,10 +57,8 @@ public class Labyrinth {
 //				}
 				
 				//Position Schlüssel
-=======
 			
 				//Position Schlüssel
->>>>>>> branch 'master' of https://github.com/HindiBones/project.git
 				zufallsZahl = (Math.random()*zaehler)-1;
 				int zz2 = (int) zufallsZahl;
 				while (zz2 == zz1){
