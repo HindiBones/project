86f2659974c2092af81b9f46a8e67da06a69047e
Marius <Marius@Desktop-PC.Speedport_W_723V_1_40_000>
2016-07-12 23:16:06 +0200

    .

--- src/pp2016/team13/server/map/Koordinaten.java
+++ src/pp2016/team13/server/map/Koordinaten.java
@@ -1,21 +1,20 @@
 package pp2016.team13.server.map;
+
 /**
+ * werden gebraucht um die Positionen zu speichern x und y Werte sind int Werte
  * 
- * 
- * werden gebraucht um die Positionen zu speichern
- * x und y Werte sind int Werte
  * @author <Akbulut, Cigdem, 5382718>
  */
 public class Koordinaten {
 	public int xKoordinate;
 	public int yKoordinate;
 
-	public Koordinaten (int x , int y){
+	public Koordinaten(int x, int y) {
 		xKoordinate = x;
 		yKoordinate = y;
-		}
-	
-	public void setKoordinaten(int x, int y){
+	}
+
+	public void setKoordinaten(int x, int y) {
 		xKoordinate = x;
 		yKoordinate = y;
 	}
